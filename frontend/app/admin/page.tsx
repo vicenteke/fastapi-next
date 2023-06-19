16226df1a2eebe3a464a7f4b973d4d5faa155bd8
Vicente Knihs Erbs <vicenteknihs@gmail.com>
2023-06-19 17:08:58 -0300

    [front] feat: Table component

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -1,8 +1,24 @@
 import PermissionChecker from '@/components/PermissionChecker';
 import Button from '@/components/Button';
+import Table from '@/components/Table';
+import Icon from '@/components/Icon';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Home() {
+  const tableHeader = ['ID', 'Name', 'Points', 'Actions'];
+  const tableFooter = ['ID', 'Name', 'Points', 'Actions'];
+  const tableBody = [
+    [1, 'Player 1', 10, <Button><Icon icon={faUser} /></Button>],
+    [2, 'Player 2', 20, <Button><Icon icon={faUser} /></Button>],
+    [3, 'Player 3', 30, <Button><Icon icon={faUser} /></Button>],
+    [4, 'Player 4', 40, null],
+    [5, 'Player 5', 50, <Button><Icon icon={faUser} /></Button>],
+    [6, 'Player 6', 60, <Button><Icon icon={faUser} /></Button>],
+    [7, 'Player 7', 70, <Button><Icon icon={faUser} /></Button>],
+  ];
+
+
   return (
     <>
       <PermissionChecker permissions={['IS_ADMIN']} redirect>
@@ -15,6 +31,14 @@ export default function Home() {
         <Button href='/'>
           Home
         </Button>
+        <Table
+          header={tableHeader}
+          footer={tableFooter}
+          body={tableBody}
+          activeRow={2}
+          idColumn={0}
+          isFullwidth={false}
+        />
       </PermissionChecker>
     </>
   )
---
+++ frontend/components/Table.tsx
@@ -0,0 +1,100 @@
+interface Props extends React.ComponentPropsWithoutRef<'table'> {
+  header?: Array<React.ReactNode | string | null>
+  footer?: Array<React.ReactNode | string | null>
+  body?: Array<Array<React.ReactNode | string | null>>
+  activeRow?: number
+  idColumn?: number
+  isBordered?: boolean
+  isStriped?: boolean
+  isNarrow?: boolean
+  isHoverable?: boolean
+  isFullwidth?: boolean
+}
+
+/* Description: Bulma table wrapper.
+ *
+ * Props (also includes NextImage props):
+ * - header: array with the header elements;
+ * - footer: array with the footer elements;
+ * - body: array of arrays with the body elements;
+ * - activeRow: index of the active row;
+ * - idColumn: index of the ID column;
+ * - isBordered: whether to make the table bordered or not;
+ * - isStriped: whether to make the table striped or not;
+ * - isNarrow: whether to make the table narrowed or not;
+ * - isHoverable: whether to make the table hoverable or not;
+ * - isFullwidth: whether to make the table full-width or not;
+ */
+function Table({
+  className,
+  header,
+  footer,
+  body,
+  activeRow,
+  idColumn,
+  isBordered,
+  isStriped,
+  isNarrow,
+  isHoverable,
+  isFullwidth=true,
+  ...props
+}: Props) {
+  let classNames = [className, 'table'];
+  if (isBordered) classNames.push('is-bordered');
+  if (isStriped) classNames.push('is-striped');
+  if (isNarrow) classNames.push('is-narrow');
+  if (isHoverable) classNames.push('is-hoverable');
+  if (isFullwidth) classNames.push('is-fullwidth');
+
+  const bodyData = body === undefined ? [] : body.map((line, lineIndex) => {
+    return (
+    <tr
+      key={lineIndex}
+      className={activeRow && lineIndex === activeRow ? 'is-selected' : ''}
+    >
+      {line.map((item, index) => {
+        if (idColumn && index === idColumn)
+          return <th key={index}>{item}</th>
+        return <td key={index}>{item}</td>
+      })}
+    </tr>)
+  })
+
+  return (
+  <table className={classNames.join(' ')} {...props}>
+    {header &&
+      <thead>
+        <tr>
+          {header.map((item, index) => <th key={index}>{item}</th>)}
+        </tr>
+      </thead>
+    }
+    {footer &&
+      <tfoot>
+        <tr>
+          {footer.map((item, index) => <th key={index}>{item}</th>)}
+        </tr>
+      </tfoot>
+    }
+    {body &&
+      <tbody>
+        {body.map((line, lineIndex) => {
+          return (
+            <tr
+              key={lineIndex}
+              className={activeRow && lineIndex === activeRow ? 'is-selected' : ''}
+            >
+              {line.map((item, index) => {
+                if (idColumn && index === idColumn)
+                  return <th key={index}>{item}</th>
+                return <td key={index}>{item}</td>
+              })}
+          </tr>)
+        })}
+      </tbody>
+    }
+  </table>
+  )
+}
+
+export default Table;
